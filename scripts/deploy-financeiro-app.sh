7af4ed8cb9cdd13ac1ac4f2564ca0d237944f266
Alexandre Melo <asmelo10@hotmail.com>
2020-02-19 08:54:54 -0300

    Fix deploy file

--- scripts/deploy-financeiro-app.sh
+++ scripts/deploy-financeiro-app.sh
@@ -2,4 +2,4 @@
 cd ..
 sudo npm run build
 sudo rm -r /usr/share/nginx/html/*
-sudo cp /home/ec2-user/projetos/financeiro-app/dist/* /usr/share/nginx/html
+sudo cp -r /home/ec2-user/projetos/financeiro-app/dist/* /usr/share/nginx/html
