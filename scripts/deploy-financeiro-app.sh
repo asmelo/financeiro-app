59d1ce13de8f699c8fca229114fc76a39f309495
Alexandre Melo <asmelo10@hotmail.com>
2020-02-20 07:56:02 -0300

    change deploy-financeiro-app script

--- scripts/deploy-financeiro-app.sh
+++ scripts/deploy-financeiro-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ..
+cd /home/ec2-user/projetos/financeiro-app
 sudo npm run build
 sudo rm -r /usr/share/nginx/html/*
 sudo cp -r /home/ec2-user/projetos/financeiro-app/dist/* /usr/share/nginx/html
