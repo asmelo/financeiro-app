96dac19547a236ed7911551d56c7a3724f0dd858
Alexandre Melo <asmelo10@hotmail.com>
2020-04-14 22:34:39 -0300

    Execute npm install in build

--- scripts/deploy-financeiro-app.sh
+++ scripts/deploy-financeiro-app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/projetos/financeiro-app
+npm install
 sudo npm run build
 sudo rm -r /usr/share/nginx/html/*
 sudo cp -r /home/ec2-user/projetos/financeiro-app/dist/* /usr/share/nginx/html
