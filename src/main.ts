80c1f184a518ab26439895404b245ef2d5e70ec1
Alexandre Melo <asmelo10@hotmail.com>
2020-04-16 22:29:32 -0300

    Config vuetify

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import keycloak from '@/services/keycloak.service'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
-const initializeApp = (): void => {
-  new Vue({
-    router,
-    store,
-    vuetify: Vuetify,
-    render: h => h(App),
-  }).$mount('#app');
-};
-
-keycloak.init({ onLoad: 'login-required' }).success((authenticated: boolean) => {
-  if (!authenticated) return;
-  initializeApp();
-});
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#app');
